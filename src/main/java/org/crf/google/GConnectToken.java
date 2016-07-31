b86462ba706dbdb1852e9ab79e383b915fa315c9
Sebastien Lavayssiere <sebastien.lavayssiere@gmail.com>
2016-07-31 19:47:11 +0200

    change credentials

--- src/main/java/org/crf/google/GConnectToken.java
+++ src/main/java/org/crf/google/GConnectToken.java
@@ -9,6 +9,7 @@ import java.util.List;
 import com.amazonaws.AmazonClientException;
 import com.amazonaws.auth.AWSCredentials;
 import com.amazonaws.auth.profile.ProfileCredentialsProvider;
+import com.amazonaws.auth.profile.internal.ProfileAssumeRoleCredentialsProvider;
 import com.amazonaws.regions.Region;
 import com.amazonaws.regions.Regions;
 import com.amazonaws.services.s3.AmazonS3;
@@ -63,18 +64,19 @@ public class GConnectToken {
     }
     
     private S3Object getS3Data(){
-    	AWSCredentials credentials = null;
-        try {
-            credentials = new ProfileCredentialsProvider().getCredentials();
-        } catch (Exception e) {
-            throw new AmazonClientException(
-                    "Cannot load the credentials from the credential profiles file. " +
-                    "Please make sure that your credentials file is at the correct " +
-                    "location (~/.aws/credentials), and is in valid format.",
-                    e);
-        }
+//    	AWSCredentials credentials = null;
+//        try {
+//            credentials = new ProfileCredentialsProvider().getCredentials();
+//        } catch (Exception e) {
+//            throw new AmazonClientException(
+//                    "Cannot load the credentials from the credential profiles file. " +
+//                    "Please make sure that your credentials file is at the correct " +
+//                    "location (~/.aws/credentials), and is in valid format.",
+//                    e);
+//        }
         
-        AmazonS3 s3 = new AmazonS3Client(credentials);
+        //AmazonS3 s3 = new AmazonS3Client(credentials);
+        AmazonS3 s3 = new AmazonS3Client();
         Region euWest1 = Region.getRegion(Regions.EU_WEST_1);
         s3.setRegion(euWest1);
         
