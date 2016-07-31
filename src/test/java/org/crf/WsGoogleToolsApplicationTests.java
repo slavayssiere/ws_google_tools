eaa0c609d19790aaf19546b787b37cb3d699a924
Sebastien Lavayssiere <sebastien.lavayssiere@gmail.com>
2016-07-31 18:58:13 +0200

    update

--- src/test/java/org/crf/WsGoogleToolsApplicationTests.java
+++ src/test/java/org/crf/WsGoogleToolsApplicationTests.java
@@ -2,15 +2,4 @@ package org.crf;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.springframework.boot.test.SpringApplicationConfiguration;
-import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
-@RunWith(SpringJUnit4ClassRunner.class)
-@SpringApplicationConfiguration(classes = WsGoogleToolsApplication.class)
-public class WsGoogleToolsApplicationTests {
-
-	@Test
-	public void contextLoads() {
-	}
-
-}
